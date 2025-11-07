fb240e71f101a490d62b3269e7054bc89a5fdb3f
Lawrence <lawrencesanzogni@gmail.com>
2025-11-06 19:35:31 -0600

    Addition: Created 'binned' datasets where financial and demographic are grouped based on whether they show a positive trend.
    Addition: Added modules to save datasets as csv files for formatting and readability.

--- SiteScan/panda.py
+++ SiteScan/panda.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from PIL.ImageChops import composite
-from pandas.core.interchange import dataframe
+import csv
 
 class PandaDataframe:
 
     dataframe = {}
+    binned_dataframe = []
 
     def __init__(self):
         return
@@ -26,11 +26,11 @@ class PandaDataframe:
         self.dataframe = self.dataframe.round(2)
 
     def fill_discretes(self):
-        col_list = self.dataframe.columns
+        cont_cols = self.dataframe.columns
         discrete_cols = ['pop_increase', 'income_increase', 'home_value_increase', 'commute_time_increase', 'poverty_increase']
-        for i, col in enumerate(col_list):
-            self.dataframe.insert(self.dataframe.columns.get_loc(col), discrete_cols[i], 1)
-            self.dataframe.loc[self.dataframe[col] < 0, discrete_cols[i]] = -1
+        for i, col in enumerate(cont_cols):
+            self.dataframe.insert(self.dataframe.columns.get_loc(col), discrete_cols[i], True)
+            self.dataframe.loc[self.dataframe[col] < 0, discrete_cols[i]] = False
 
     def readd_keys(self):
         composite_keys_dataframe = pd.DataFrame(pd.read_csv(
@@ -38,16 +38,43 @@ class PandaDataframe:
         self.dataframe.insert(0, 'Year', composite_keys_dataframe['Year'])
         self.dataframe.insert(1, 'Zip Code', composite_keys_dataframe['Zip Code'])
 
+    def filter_cont_cols(self):
+        self.dataframe = self.dataframe.drop(columns=['Population_diff', 'Income_diff', 'Home Value_diff', 'Commute Time_diff', 'Poverty_diff'])
+
     def save_dataframe(self):
         self.dataframe.to_csv('discrete_dataset.csv')
         print('dataframe saved')
         return
+
+    def bin_items(self):
+        dataframe  = self.dataframe
+        cols = dataframe.columns
+        itemsets = []
+        for index, row in dataframe.iterrows():
+            basket = []
+            for i, item in enumerate(row):
+                if item:
+                    basket.append(cols[i])
+            itemsets.append(basket)
+        self.binned_dataframe = itemsets
+
+
+    def save_binned_dataframe(self):
+        with open('binned_dataset.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(self.binned_dataframe)
+        return
+
+
 
 def main():
     panda = PandaDataframe()
     panda.load_dataframe()
     panda.to_percentages(panda.dataframe)
     panda.fill_discretes()
+    panda.filter_cont_cols()
+    panda.bin_items()
+    panda.save_binned_dataframe()
     panda.readd_keys()
     panda.save_dataframe()
     return
