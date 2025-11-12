ccba655187935d012653475dec437aa580b1fa49
Lawrence <lawrencesanzogni@gmail.com>
2025-11-12 15:19:25 -0600

    Addition: Split datasets into INCREASE and DECREASE datasets for a more thorough association evaluation

--- SiteScan/Pandas.py
+++ SiteScan/Pandas.py
@@ -5,6 +5,8 @@ class PandaDataframe:
 
     dataframe = {}
     binned_dataframe = []
+    increased_dataframe = []
+    decreased_dataframe = []
 
     def __init__(self):
         return
@@ -35,42 +37,81 @@ class PandaDataframe:
         print('saving percentages to csv file...')
         df.to_csv('csv/grouped_dataset_percentages.csv')
 
-
     def discretize(self):
         print('converting continuous variables to discrete variables...')
         cont_cols = self.dataframe.columns
-        for i, col in enumerate(cont_cols):
-            self.dataframe.loc[self.dataframe[col] > 0, col] = True
-            self.dataframe.loc[self.dataframe[col] < 0, col] = False
-        print('saving discrete dataframe to csv file...')
-        self.dataframe.to_csv('csv/discrete_dataset.csv')
+
+        def discretize_increase():
+            increase_data = self.dataframe.copy()
+            for i, col in enumerate(cont_cols):
+                increase_data[col] = (increase_data[col] > 0).astype(int)
+                increase_data.loc[increase_data[col] > 0, col] = 1
+                increase_data.loc[increase_data[col] < 0, col] = 0
+            print('saving increasing discrete dataframe to csv file...')
+            increase_data.to_csv('csv/increase/increase_discrete_dataset.csv')
+            return increase_data
+
+        self.increased_dataframe = discretize_increase()
+
+        def discretize_decrease():
+            decrease_data = self.dataframe.copy()
+            for i, col in enumerate(cont_cols):
+                decrease_data.loc[decrease_data[col] > 0, col] = 0
+                decrease_data.loc[decrease_data[col] < 0, col] = 1
+            print('saving decreasing discrete dataframe to csv file...')
+            decrease_data.to_csv('csv/decrease/decrease_discrete_dataset.csv')
+            return decrease_data
+
+        self.decreased_dataframe =  discretize_decrease()
 
     def bin_items(self):
-        print('binning items')
-        dataframe  = self.dataframe
-        cols = dataframe.columns
-        itemsets = []
-        for index, row in dataframe.iterrows():
-            basket = []
-            for i, item in enumerate(row):
-                if item:
-                    basket.append(cols[i])
-            itemsets.append(basket)
-        self.binned_dataframe = itemsets
+        print('binning items...')
 
-        print('saving binned dataframe...')
-        with open('csv/binned_dataset.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(self.binned_dataframe)
+        def bin_increase():
+            dataframe = self.increased_dataframe.copy()
+            cols = dataframe.columns
+            print(cols)
+            itemsets = []
+            for index, row in dataframe.iterrows():
+                basket = []
+                for i, item in enumerate(row):
+                    if item == 1.0:
+                        basket.append(cols[i])
+                itemsets.append(basket)
+
+            print('saving increase binned dataframe...')
+            with open('csv/increase/binned_dataset_increase.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(itemsets)
+
+        bin_increase()
+
+        def bin_decrease():
+            dataframe = self.decreased_dataframe.copy()
+            cols = dataframe.columns
+            itemsets = []
+            for index, row in dataframe.iterrows():
+                basket = []
+                for i, item in enumerate(row):
+                    if item == 1.0:
+                        basket.append(cols[i])
+                itemsets.append(basket)
+            print(itemsets)
+            print('saving decreased binned dataframe...')
+
+            with open('csv/decrease/binned_dataset_decrease.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(itemsets)
+        bin_decrease()
 
     def print(self):
         print(self.dataframe)
 
-# def main():
-#     panda = PandaDataframe()
-#     panda.load_dataframe()
-#     panda.to_percentages()
-#     panda.discretize()
-#     panda.bin_items()
-#     return
-# main()
+def main():
+    panda = PandaDataframe()
+    panda.load_dataframe()
+    panda.to_percentages()
+    panda.discretize()
+    panda.bin_items()
+    return
+main()
--- SiteScan/Unsupervised_Model.py
+++ SiteScan/Unsupervised_Model.py
@@ -5,26 +5,53 @@ from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 
 class UnsupervisedModel:
-    data = []
+    increase_data = []
+    decrease_data = []
+
     def __init__(self):
         return
 
     def load_data(self):
-        with open('csv/binned_dataset.csv', 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            self.data = list(reader)
+
+        def load_increase_data():
+            print('loading increase data...')
+            with open('csv/increase/binned_dataset_increase.csv', 'r') as csvfile:
+                print('loading increase data...')
+                reader = csv.reader(csvfile)
+                self.increase_data = list(reader)
+        load_increase_data()
+
+        def load_decrease_data():
+            print('loading decrease data...')
+            with open('csv/decrease/binned_dataset_decrease.csv', 'r') as csvfile:
+                print('loading decrease data...')
+                reader = csv.reader(csvfile)
+                self.decrease_data = list(reader)
+        load_decrease_data()
 
     def transaction_encoder(self):
         transaction_encoder = TransactionEncoder()
-        transaction_encoder_array = transaction_encoder.fit(self.data).transform(self.data)
-        dataframe = pd.DataFrame(transaction_encoder_array, columns=transaction_encoder.columns_)
-        self.data = dataframe
+
+        def encode_increase():
+            print('encoding increase data...')
+            transaction_encoder_array = transaction_encoder.fit(self.increase_data).transform(self.increase_data)
+            dataframe = pd.DataFrame(transaction_encoder_array, columns=transaction_encoder.columns_)
+            self.increase_data = dataframe
+        encode_increase()
+
+        def encode_decrease():
+            print('encoding decrease data...')
+            transaction_encoder_array = transaction_encoder.fit(self.decrease_data).transform(self.decrease_data)
+            dataframe = pd.DataFrame(transaction_encoder_array, columns=transaction_encoder.columns_)
+            self.decrease_data = dataframe
+        encode_decrease()
 
     def print(self):
-        print(self.data)
+        print(self.increase_data)
+        print(self.decrease_data)
 
     def apriori_model(self):
-        apriori_itemsets = apriori(self.data, min_support=0.1, use_colnames=True)
+        apriori_itemsets = apriori(self.increase_data, min_support=0.1, use_colnames=True)
         apriori_itemsets['length'] = apriori_itemsets['itemsets'].apply(lambda x: len(x))
         # apriori_itemsets = apriori_itemsets[(apriori_itemsets['length'] >= 2) &
         #                   (apriori_itemsets['support'] >= 0.4)]
@@ -33,17 +60,36 @@ class UnsupervisedModel:
         return apriori_itemsets
 
     def association_rules(self):
-        apriori_itemsets = apriori(self.data, min_support=0.1, use_colnames=True)
-        rules = association_rules(apriori_itemsets, metric="confidence", min_threshold=0.1)
-        rules = rules[rules['antecedents'].apply(lambda x: len(x) == 1) & rules['consequents'].apply(lambda x: len(x) == 1)]
-        print("Association Rules:", rules.shape[0])
-        rules['support'] = round(rules['support'] * 100, 2)
-        rules['confidence'] = round(rules['confidence'] * 100, 2)
-        rules['lift'] = round(rules['lift'] * 100, 2)
-        rules = rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
-        rules.sort_values(by='lift', ascending=False, inplace=True)
-        rules = rules.head(10)
-        rules.to_csv('csv/association_rules.csv')
+
+        def increase_association_rules():
+            print('generating increase association rules...')
+            apriori_itemsets = apriori(self.increase_data, min_support=0.1, use_colnames=True)
+            rules = association_rules(apriori_itemsets, metric="confidence", min_threshold=0.1)
+            rules = rules[rules['antecedents'].apply(lambda x: len(x) == 1) & rules['consequents'].apply(lambda x: len(x) == 1)]
+            print("Association Rules:", rules.shape[0])
+            rules['support'] = round(rules['support'] * 100, 2)
+            rules['confidence'] = round(rules['confidence'] * 100, 2)
+            rules['lift'] = round(rules['lift'] * 100, 2)
+            rules = rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+            rules.sort_values(by='lift', ascending=False, inplace=True)
+            rules = rules.head(10)
+            rules.to_csv('csv/increase/increase_association_rules.csv')
+        increase_association_rules()
+
+        def decrease_association_rules():
+            print('generating decrease association rules...')
+            apriori_itemsets = apriori(self.decrease_data, min_support=0.1, use_colnames=True)
+            rules = association_rules(apriori_itemsets, metric="confidence", min_threshold=0.1)
+            rules = rules[rules['antecedents'].apply(lambda x: len(x) == 1) & rules['consequents'].apply(lambda x: len(x) == 1)]
+            print("Association Rules:", rules.shape[0])
+            rules['support'] = round(rules['support'] * 100, 2)
+            rules['confidence'] = round(rules['confidence'] * 100, 2)
+            rules['lift'] = round(rules['lift'] * 100, 2)
+            rules = rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+            rules.sort_values(by='lift', ascending=False, inplace=True)
+            rules = rules.head(10)
+            rules.to_csv('csv/decrease/decrease_association_rules.csv')
+        decrease_association_rules()
 
     def save_model(self):
         data = self.apriori_model()
@@ -57,6 +103,6 @@ def main():
     model.apriori_model()
     # print(model.apriori_model())
     model.association_rules()
-    model.save_model()
+    # model.save_model()
     return
 main()
