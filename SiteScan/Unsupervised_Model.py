17043a36eb30a16a9568e417d8befa618127aaf1
Lawrence <lawrencesanzogni@gmail.com>
2025-11-12 11:00:29 -0600

    Addition: Added apriori association methods to generate support, confidence, and lift data that's saved in a CSV

--- SiteScan/Unsupervised_Model.py
+++ SiteScan/Unsupervised_Model.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+from mlxtend.evaluate import lift_score
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 
@@ -23,12 +24,26 @@ class UnsupervisedModel:
         print(self.data)
 
     def apriori_model(self):
-        apriori_itemsets = apriori(self.data, min_support=0.4, use_colnames=True)
+        apriori_itemsets = apriori(self.data, min_support=0.1, use_colnames=True)
         apriori_itemsets['length'] = apriori_itemsets['itemsets'].apply(lambda x: len(x))
-        apriori_itemsets = apriori_itemsets[(apriori_itemsets['length'] >= 2) &
-                          (apriori_itemsets['support'] >= 0.4)]
+        # apriori_itemsets = apriori_itemsets[(apriori_itemsets['length'] >= 2) &
+        #                   (apriori_itemsets['support'] >= 0.4)]
         apriori_itemsets = apriori_itemsets.sort_values(by='support', ascending=False)
+
         return apriori_itemsets
+
+    def association_rules(self):
+        apriori_itemsets = apriori(self.data, min_support=0.1, use_colnames=True)
+        rules = association_rules(apriori_itemsets, metric="confidence", min_threshold=0.1)
+        rules = rules[rules['antecedents'].apply(lambda x: len(x) == 1) & rules['consequents'].apply(lambda x: len(x) == 1)]
+        print("Association Rules:", rules.shape[0])
+        rules['support'] = round(rules['support'] * 100, 2)
+        rules['confidence'] = round(rules['confidence'] * 100, 2)
+        rules['lift'] = round(rules['lift'] * 100, 2)
+        rules = rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+        rules.sort_values(by='lift', ascending=False, inplace=True)
+        rules = rules.head(10)
+        rules.to_csv('csv/association_rules.csv')
 
     def save_model(self):
         data = self.apriori_model()
@@ -40,7 +55,8 @@ def main():
     model.load_data()
     model.transaction_encoder()
     model.apriori_model()
-    print(model.apriori_model())
+    # print(model.apriori_model())
+    model.association_rules()
     model.save_model()
     return
 main()
